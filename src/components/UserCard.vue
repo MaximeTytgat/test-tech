4ecc798af921c8cdd2de0d2de26561a7dd769965
Maxime Tytgat <mty@apik.cloud>
2023-04-25 16:40:26 +0200

    [FEA]: Setup router, view, component, base.css, colors and remove VueWelcome content

---
+++ src/components/UserCard.vue
@@ -0,0 +1,12 @@
+<script setup lang="ts">
+  
+</script>
+
+<template>
+
+  Lala
+
+</template>
+
+<style scoped lang="scss">
+</style>
---
+++ src/views/UserView.vue
@@ -0,0 +1,10 @@
+<script setup lang="ts">
+  import UserCard from '../components/UserCard.vue'
+</script>
+
+<template>
+  <UserCard/>
+</template>
+
+<style scoped lang="scss">
+</style>
