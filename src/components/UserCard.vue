d0d6a189952bd1bb09c713b7f27dc57d71c93476
Maxime Tytgat <mty@apik.cloud>
2023-04-25 17:03:17 +0200

    [FEA]: add User Store in UserView with functions. [FEA]: add edit and remove fonction to UserCard, add border color function

--- src/components/UserCard.vue
+++ src/components/UserCard.vue
@@ -1,11 +1,23 @@
 <script setup lang="ts">
+  import type { User } from '../stores/user'
+  
+  defineProps<{
+    user: User
+  }>()
+
+  const edit = (username: User["name"]): void => console.log(`Edit ${username.first} ${username.last}`);
+  const remove = (username: User["name"]): void => console.log(`Remove ${username.first} ${username.last}`);
+  
+  const borderColor = (gender: User["gender"]): string => gender === 'male' ? '#60547A' : '#F9E859';
   
 </script>
 
 <template>
-
-  Lala
-
+  <div>
+    <h3 >{{ user.name.first }} {{ user.name.last }} {{ borderColor(user.gender) }}</h3>
+    <button @click="edit(user.name)">edit</button>
+    <button @click="remove(user.name)">remove</button>
+  </div>
 </template>
 
 <style scoped lang="scss">
--- src/views/UserView.vue
+++ src/views/UserView.vue
@@ -1,9 +1,30 @@
 <script setup lang="ts">
   import UserCard from '../components/UserCard.vue'
+  import { useUserStore } from '../stores/user';
+
+  const userStore = useUserStore();
+
+  const fetchUsers = (): void => {
+    userStore.fetchUsers();
+  };
+
+  const AddUser = (): void => {
+    userStore.AddUser();
+  };
+
+  fetchUsers();
 </script>
 
 <template>
-  <UserCard/>
+  <div class="user-view-container">
+    <div class="title-container">
+      <h1 class="title">Liste des utilisateurs</h1>
+      <button @click="AddUser">Ajouter un utilisateur</button>
+    </div>
+    <div class="user-list">
+      <UserCard v-for="user in userStore.users" :user="user" />
+    </div>
+  </div>
 </template>
 
 <style scoped lang="scss">
